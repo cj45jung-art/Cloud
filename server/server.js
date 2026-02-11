bba96cfd8f89615b5f09b4633a4c87bcdd38ace2
junguk <cj45jung@gmail.com>
2026-02-11 10:36:21 +0900

    init

---
+++ server/server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+
+const indexRouter = require('./routes/index')
+const path = require('path')
+
+//cors 오류 해결
+const cors = require('cors')
+app.use(cors())
+app.use(express.json())
+
+//post 방식 데이터를 주고 받을 떄 필요한 미들 웨어
+app.use(express.urlencoded({extended : true}))
+
+// npm run build 를 통해 React 프로젝트를
+// 하나의 index.html 로 만들었음
+//  => 그 파일을 키기 위해 정적인 파일을 담당하는 코드 
+app.use(express.static(path.join('../client/dist')))
+
+app.use('/', indexRouter)
+
+app.set('port', process.env.PORT || 3000)
+app.listen(app.get('port'), ()=>{
+    console.log(`${app.get('port')}번 포트에서 대기 중...`)
+})
